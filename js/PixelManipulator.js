902b291da09013fba05d2a1137ed319c3da4c87b
Nishant Kaushik <nishant.kaushik@accolite.com>
2018-04-05 00:29:37 +0530

    front end working

---
+++ js/PixelManipulator.js
@@ -0,0 +1,184 @@
+function Manipulator(buffer, width, height){
+    this.buffer = buffer;
+
+    this.width = width;
+    this.height = height;
+    this.buf8 = new Uint8ClampedArray(buffer);
+    this.buf32 = new Uint32Array(buffer);
+    // this.isLittleEndian = true;
+    // this.getEndianness = function() {
+        // // Determine whether Uint32 is little- or big-endian
+        // // https://en.wikipedia.org/wiki/Endianness
+        // var temp = this.buf32[0];
+        // this.buf32[0] = 0x0a0b0c0d;
+        // this.isLittleEndian = !(this.buf8[0] === 0x0a && this.buf8[1] === 0x0b && this.buf8[2] === 0x0c &&
+                                // this.buf8[3] === 0x0d);
+        // // reset the change
+        // this.buf32[0] = temp;
+    // };
+    // this.getEndianness();
+	
+    // // All encoding of the image goes in here
+    // // see http://jsperf.com/canvas-pixel-manipulation for speed test
+    this.encode = function(text){
+		
+		// var strr = "";
+		// for(var i=0; i<50; ++i)
+			// strr+= this.buf32[i].toString(2)+', ';
+		// console.log(strr);
+		
+        var index = 0;
+        var binary = "";
+        for(index; index < text.length; index++){
+            binary += this.stringToBinary(text.charCodeAt(index));
+        }
+        // pad extra 0's till binary is a multiple of 3
+        if(binary.length % 3 !== 0){
+            var inner = 0;
+            for(inner; inner < binary.length % 3; inner++){
+                binary += "0";
+            }
+        }
+        // actually encode the string
+        index = 0;
+        var pixelIndex = 0;
+        for(index; index < binary.length; index+=3) {
+            var bits = [];
+            // 48 is the char code for 0, 49 for 1
+            // puts in a number 1 or 0 based on the string "1" or "0"
+            bits.push(binary.charCodeAt(index) === 48 ? 0 : 1);
+            bits.push(binary.charCodeAt(index + 1) === 48 ? 0 : 1);
+            bits.push(binary.charCodeAt(index + 2) === 48 ? 0 : 1);
+
+            this.setLsb(pixelIndex, bits);
+            pixelIndex+=4;
+        }
+        // set the LSB of all remaining pixels to 0, causing them to output
+        // nothing when decoding
+        for(pixelIndex; pixelIndex < this.buf8.length; pixelIndex+=4){
+            this.setLsb(pixelIndex, [0,0,0]);
+        }
+		// var strr = "";
+		// for(var i=0; i<50; ++i)
+			// strr+= this.buf32[i].toString(2)+', ';
+		// console.log(strr);
+    };
+
+    /*
+    Decodes the buffer stored in memory for the PixelManipulator
+    */
+    this.decode = function() {
+        var finalBinary;
+        var finalArray = [];
+        var i = 0;
+        for (i; i < this.buf8.length; i+=4) {
+            var tempArray = this.getLsb(i);
+            var j = 0;
+            for (j; j < tempArray.length; j++) {
+                finalArray.push(tempArray[j]);
+            }
+        }
+        finalBinary = this.arrayToBinary(finalArray);
+		//console.log(this.convertBinaryToString(finalBinary));
+        return this.convertBinaryToString(finalBinary);
+    };
+
+    /*
+    Converts a given string to a binary string
+    */
+    this.stringToBinary = function(charcode) {
+        var padded = "00000000";
+        // pad the values if necessary (usually is necessary)
+        var unpaddedBin = charcode.toString(2);
+        return padded.substring(0, 8 - unpaddedBin.length) + unpaddedBin;
+    };
+
+    this.arrayToBinary = function (array) {
+        var binaryString = '';
+        var spacecount = 0;
+        for (var i = 0; i < array.length; i++) {
+            binaryString += array[i];
+            spacecount++;
+            if (spacecount == 8) {
+                binaryString += " ";
+                spacecount = 0;
+            }
+        }
+        return binaryString;
+    };
+
+    /*
+    Converts the binary array given back to a string
+    */
+    this.convertBinaryToString = function(binary) {
+        var returnValue = "";
+        var res = binary.split(" ");
+        for (var i = 0; i < res.length; i++)
+        {
+            var value  = parseInt(res[i],2).toString(10);
+            returnValue += String.fromCharCode(value);
+        }
+        return returnValue;
+    };
+
+    // Sets the pixel at location (x,y) to the given RGBA value
+    //assumes that the given (x,y) coordinate is within the image
+    // this.setPixel = function(index, r, g, b, a){
+        // if(this.isLittleEndian){
+            // this.buf32[index] = (a << 24) | (b << 16) | (g << 8) | r;
+         // } else {
+            // this.buf32[index] = (r << 24) | (g << 16) | (b << 8) | a;
+         // }
+    // };
+	//var times=0;
+    /* Sets the lsb of the pixel at the given index with the given array of bits */
+    this.setLsb = function(index, bits){
+        var i = 0;
+        for(i; i < bits.length; i++){
+            // for once I actually want == not ===
+			// if(times<=100) {
+			// 	times++;
+			// 	console.log(i + ' Setting: ' + bits[i]);
+			// 	if(bits[i] == 0){
+			// 		// clear bit, making it 0
+			// 		console.log('old: ' + this.buf8[index].toString(2));
+			// 		this.buf8[index] &= ~(1); // x = x & ~(1 << i*8) 
+			// 		console.log('new: ' + this.buf8[index].toString(2));
+			// 	} else {
+			// 		// or bit, making it a 1
+			// 		console.log('old: ' + this.buf8[index].toString(2));
+			// 		this.buf8[index] |= 1;
+			// 		console.log('new: ' + this.buf8[index].toString(2));
+			// 	}
+			// }
+			// else {
+				if(bits[i] == 0){
+					// clear bit, making it 0
+					this.buf8[index] &= ~(1); // x = x & ~(1 << i*8) 
+				} else {
+					// or bit, making it a 1
+					this.buf8[index] |= 1;
+				}
+			// }
+			index++;
+        }
+    };
+
+    /*
+     gets the LSB of each of the 3 color channels.
+     Returns an array of 3 1's or 0's representing the 3 LSBs
+    */
+    this.getLsb = function(index){
+        var result = [];
+        // &'ing with 1 will return the LSB
+        // red
+        result.push((this.buf8[index]) & 1);
+		result.push((this.buf8[index+1]) & 1);
+		result.push((this.buf8[index+2]) & 1);
+        // blue
+        //result.push((this.buf32[index] >> 8) & 1);
+        // green
+        //result.push((this.buf32[index] >> 16) & 1);
+        return result;
+    };
+}
---
+++ js/app.js
@@ -0,0 +1,365 @@
+angular.module("Stego", ["ui.router","webcam"])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state("login", {
+                url: "/login",
+                templateUrl: "views/login.html",
+                controller: "StegoController"
+            })
+            .state("signup", {
+                url: "/signUp",
+                templateUrl: "views/signup1.html",
+                controller: "StegoController"
+            })
+            .state("signup2", {
+                url: "/signUp2",
+                templateUrl: "views/signup2.html",
+                controller: "StegoController",
+                params: {
+                	userdata: null
+                }
+            })
+            .state("home", {
+                url: "/home",
+                templateUrl: "views/homepage.html",
+                controller: "StegoController"
+            })
+            .state("welcome", {
+                url: "/welcome",
+                templateUrl: "views/welcome.html",
+                controller: "StegoController"
+            });
+            $urlRouterProvider.otherwise("/welcome");
+    })
+    .controller("StegoController", function($scope, $state, $stateParams, $http, $timeout){
+        "use strict";
+        $stateParams.counter =1;
+        $scope.faceimgLogin = 0;
+        $scope.availability= false;
+        $scope.show = false;
+        $scope.regimg = [];
+        $scope.decode = function () {
+            var file = document.getElementById("image-input").files[0];
+            if(file){
+                var read = new FileReader();
+                read.onloadend = function(e){
+                    //get the url result
+                    var result = read.result;
+                    // put that url in an img element
+                    var imageElement = document.createElement("img");
+                    imageElement.setAttribute("src", result);
+
+                    // put that image into a canvas
+                    var canvas = document.createElement('canvas');
+                    canvas.setAttribute("width", imageElement.naturalWidth);
+                    canvas.setAttribute("height", imageElement.naturalHeight);
+                    var context = canvas.getContext('2d');
+                    context.drawImage(imageElement, 0, 0);
+                    var rawData = context.getImageData(0, 0, canvas.width, canvas.height);
+                    // getting a reference to the data to speed up references
+                    var data = rawData.data;
+                    var manipulator = new Manipulator(data.buffer, canvas.width, canvas.height);
+                    var output = manipulator.decode();
+                    rawData.data.set(manipulator.buf8);
+                    context.putImageData(rawData, 0, 0);
+                    document.getElementById("output-text").innerHTML = output;
+                    $scope.loading = false;
+                };
+                $scope.loading = true;
+                read.readAsDataURL(file);
+            }
+        };
+
+        $scope.goHome = function() {
+            $state.go('welcome');
+        };
+
+        $scope.goToLogin = function() {
+            $state.go('login');
+        };
+
+        $scope.goToSignUp = function() {
+            $state.go('signup');
+        };
+        $scope.goToSignUp2 = function(){
+        	if($scope.user == null || $scope.user == undefined || $scope.user == '' || 
+        		$scope.dob == null || $scope.dob == undefined || $scope.dob == '' || 
+        		$scope.pass == null || $scope.pass == undefined || $scope.pass == '' || 
+        		$scope.name == null || $scope.name == undefined || $scope.name == ''){
+        		alert('Please fill all the details before proceeding forward.');
+        		return;
+        	}
+        	$scope.checkUsername();
+        	if($scope.availability == false){
+        		alert('Please choose a different username');
+        		return;
+        	}
+        	var userdata = $scope.user + '@@' + $scope.pass + '@@' + $scope.age + '@@' + $scope.dob + '@@' + $scope.name ; 
+			$state.go('signup2', { userdata: userdata});
+		}
+		var _video = null,
+        patData = null;
+
+		$scope.patOpts = {x: 0, y: 0, w: 25, h: 25};
+
+		// Setup a channel to receive a video property
+		// with a reference to the video element
+		// See the HTML binding in main.html
+		$scope.channel = {};
+
+		$scope.webcamError = false;
+		$scope.onError = function (err) {
+			$scope.$apply(
+				function() {
+					$scope.webcamError = err;
+				}
+			);
+		};
+
+		$scope.onSuccess = function () {
+			// The video element contains the captured camera data
+			_video = $scope.channel.video;
+			$scope.$apply(function() {
+				$scope.patOpts.w = _video.width;
+				$scope.patOpts.h = _video.height;
+				//$scope.showDemos = true;
+			});
+		};
+
+		$scope.onStream = function (stream) {
+
+		};
+		$scope.makeSnapshot = function() {
+			if (_video) {
+				var patCanvas = document.querySelector('#faceimg');
+				if (!patCanvas) return;
+
+				patCanvas.width = _video.width;
+				patCanvas.height = _video.height;
+				var ctxPat = patCanvas.getContext('2d');
+
+				var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
+				ctxPat.putImageData(idata, 0, 0);
+				patData = idata;
+				var dataView = new DataView(idata.data.buffer);
+				var manipulator = new Manipulator(idata.data.buffer, _video.width, _video.height);
+				var username = document.getElementById("username").value;
+				var password = document.getElementById("password").value;
+				$scope.username = username;
+				$scope.password = password;
+				var encoded_text = username + '|' + password;
+				manipulator.encode(encoded_text);
+				idata.data.set(manipulator.buf8);
+				ctxPat.putImageData(idata, 0, 0);
+				$scope.imgdata = patCanvas.toDataURL().toString();
+				document.getElementById("faceimg").setAttribute("src", patCanvas.toDataURL().toString());
+			}
+		};
+
+		$scope.faceimgLoginInc = function(){
+			$scope.faceimgLogin = 1;
+		}
+
+		$scope.checkUsername = function(){
+			$scope.show = true;
+			$scope.checkUsernameCall();
+			console.log($scope.availability);
+		}
+
+		$scope.login = function() {
+			var url = 'http://127.0.0.1:8081/FaceSteg/login';
+			if($scope.faceimgLogin == 0){
+				alert('Face image not given. Please click your image and try again.');
+				return;
+			}
+			else if(document.getElementById("password") == "" || document.getElementById("username") == ""){
+				alert('Credentials field cannot be left blank');
+				return;	
+			}
+			$http({
+            method: 'POST',
+            url: url,
+            headers: {'Content-Type': 'text/plain'},
+            data: $scope.imgdata
+	        })
+	        .success(function (response) {
+	            if(response == "valid"){
+	            	$state.go('home');
+	            	alert("Successfully signed in");
+	            }
+	            else
+	            	alert("Invalid credentials");
+	        })
+	        .error(function (response) {
+	            console.log(response);
+	        });
+		}
+
+		$scope.checkUsernameCall = function() {
+			if($scope.user == "" || $scope.user == null || $scope.user == undefined ){
+				alert('Please enter usename before checking');
+				$scope.show = false;
+				return;
+			}
+			console.log($scope.user);
+			var url = 'http://127.0.0.1:8081/FaceSteg/checkUsername';
+			$http({
+            method: 'POST',
+            url: url,
+            headers: {'Content-Type': 'text/plain'},
+            data: $scope.user
+	        })
+	        .success(function (response) {
+	        	console.log(response);
+	            if(response == "yes"){
+	            	$scope.availability = true;
+	            	return true;
+	            }
+	            else{
+	            	$scope.availability = false;
+	            	return false;
+	            }
+	        })
+	        .error(function (response) {
+	            console.log(response);
+	        });
+		}
+
+		$scope.logout = function() {
+			$state.go('welcome');
+		}
+
+		$scope.signup = function() {
+
+			if($scope.regimage0 == undefined || $scope.regimage1 == undefined || $scope.regimage2 == undefined || $scope.regimage3 == undefined){
+				alert('Please click all the images!')
+				//return;
+			}
+			$scope.encodeData();
+			var data = $scope.regimage0 + '@@' + $scope.regimage1 + '@@' + $scope.regimage2 + '@@' + $scope.regimage3;
+			var url = 'http://127.0.0.1:8081/FaceSteg/signup';
+			$http({
+            method: 'POST',
+            url: url,
+            headers: {'Content-Type': 'text/plain'},
+            data: data
+	        })
+	        .success(function (response) {
+	            alert('Successfully Registered. Please Login with your username and password now.')
+	            $state.go('login');
+	        })
+	        .error(function (response) {
+	            console.log(response);
+	        });
+		}
+
+		$scope.encodeData = function() {
+			var canvas = document.getElementById('snapshot1');
+			var context = canvas.getContext('2d');
+			var idata = context.getImageData(0, 0, canvas.width, canvas.height);
+			var manipulator = new Manipulator(idata.data.buffer, _video.width, _video.height);
+			if($stateParams.userdata == undefined || $stateParams.userdata == null || $stateParams.userdata == ''){
+				alert('Please enter user data');
+				$state.go('signup');
+			}
+			manipulator.encode($stateParams.userdata);
+			idata.data.set(manipulator.buf8);
+			context.putImageData(idata, 0, 0);
+			$scope.regimage0 = canvas.toDataURL().toString();
+			//document.getElementById("snapshot1").setAttribute("src", patCanvas.toDataURL().toString());
+		}
+
+
+
+		$scope.takeSnapshot = function() {
+			var name = "#snapshot"+$stateParams.counter;
+			var index = $stateParams.counter-1;
+			$stateParams.counter++;
+			if($stateParams.counter==5)
+				$stateParams.counter=1;
+			var patCanvas = document.querySelector(name);
+			//if (!patCanvas) return;
+
+			var patCanvas2 = document.getElementById("tempImage");
+			//if (!patCanvas2) return;
+			var ctxPat2 = patCanvas2.getContext('2d');
+			var idata = ctxPat2.getImageData(0, 0, patCanvas2.width, patCanvas2.height);
+			patCanvas.width = patCanvas2.width;
+			patCanvas.height = patCanvas2.height;
+			var ctxPat = patCanvas.getContext('2d');
+			//var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
+			ctxPat.putImageData(idata, 0, 0);
+			$scope.regimg[index] = patCanvas.toDataURL.toString();
+			switch(index){
+				case 0: $scope.regimage0 = patCanvas.toDataURL().toString();
+						break;
+				case 1: $scope.regimage1 = patCanvas.toDataURL().toString();
+						break;
+				case 2: $scope.regimage2 = patCanvas.toDataURL().toString();
+						break;
+				case 3: $scope.regimage3 = patCanvas.toDataURL().toString();
+						break;
+			}
+			var img = ctxPat2.createImageData(patCanvas.width, patCanvas.height);
+			for (var i = img.data.length; --i >= 0; )
+			  img.data[i] = 0;
+			ctxPat2.putImageData(img, 0, 0);
+		};
+
+		$scope.clearAll = function() {
+			for(var j=1; j<=4; ++j) {
+				var patCanvas = document.getElementById("snapshot" + j);
+				var ctxPat = patCanvas.getContext('2d');
+				var img = ctxPat.createImageData(patCanvas.width, patCanvas.height);
+				for (var i = img.data.length; --i >= 0; )
+				  img.data[i] = 0;
+				ctxPat.putImageData(img, 0, 0);
+			}
+			$stateParams.counter = 1;
+			$scope.regimage0 = undefined;
+			$scope.regimage1 = undefined;
+			$scope.regimage2 = undefined;
+			$scope.regimage3 = undefined;
+		};
+
+		$scope.clickImage = function() {
+			if (_video) {
+				var patCanvas = document.querySelector("#tempImage");
+				if (!patCanvas) return;
+
+				patCanvas.width = _video.width;
+				patCanvas.height = _video.height;
+				var ctxPat = patCanvas.getContext('2d');
+
+				var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
+				ctxPat.putImageData(idata, 0, 0);
+			}
+		};
+		
+		/**
+		 * Redirect the browser to the URL given.
+		 * Used to download the image by passing a dataURL string
+		 */
+		$scope.downloadSnapshot = function downloadSnapshot(dataURL) {
+			window.location.href = dataURL;
+		};
+		
+		var getVideoData = function getVideoData(x, y, w, h) {
+			var hiddenCanvas = document.createElement('canvas');
+			hiddenCanvas.width = _video.width;
+			hiddenCanvas.height = _video.height;
+			var ctx = hiddenCanvas.getContext('2d');
+			ctx.drawImage(_video, 0, 0, _video.width, _video.height);
+			return ctx.getImageData(x, y, w, h);
+		};
+
+		/**
+		 * This function could be used to send the image data
+		 * to a backend server that expects base64 encoded images.
+		 *
+		 * In this example, we simply store it in the scope for display.
+		 */
+		var sendSnapshotToServer = function sendSnapshotToServer(imgBase64) {
+			$scope.snapshotData = imgBase64;
+		};
+    });
